97754b3311186bc92f56260fba87b722ee3524b8
pls148 <184445976+pls148@users.noreply.github.com>
2025-03-27 10:26:26 -0700

    Revert the idea of height and view being not one to one. Keep fixed get_leaf_chain logic

--- hotshot-query-service/src/data_source/storage/sql/transaction.rs
+++ hotshot-query-service/src/data_source/storage/sql/transaction.rs
@@ -35,7 +35,7 @@ use hotshot_types::{
     traits::{
         block_contents::BlockHeader,
         metrics::{Counter, Gauge, Histogram, Metrics},
-        node_implementation::{ConsensusTime, NodeType},
+        node_implementation::NodeType,
         EncodeBytes,
     },
 };
@@ -467,7 +467,6 @@ where
 {
     async fn insert_leaf(&mut self, leaf: LeafQueryData<Types>) -> anyhow::Result<()> {
         let height = leaf.height();
-        let view = leaf.leaf().view_number().u64();
 
         // Ignore the leaf if it is below the pruned height. This can happen if, for instance, the
         // fetcher is racing with the pruner.
@@ -517,11 +516,10 @@ where
         let qc_json = serde_json::to_value(leaf.qc()).context("failed to serialize QC")?;
         self.upsert(
             "leaf2",
-            ["height", "view", "hash", "block_hash", "leaf", "qc"],
-            ["height", "view"],
+            ["height", "hash", "block_hash", "leaf", "qc"],
+            ["height"],
             [(
                 height as i64,
-                view as i64,
                 leaf.hash().to_string(),
                 leaf.block_hash().to_string(),
                 leaf_json,
